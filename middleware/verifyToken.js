9a365afbf2b530ae658b83c81cbd24b1703323c9
Gautam Sharma <gautamsh@leewayhertz.com>
2023-03-02 10:09:24 +0530

    changes

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt=require('jsonwebtoken');
 
 
-const verifyToken=(req,res,next)=>{
+const  verifyToken=(req,res,next)=>{
     const token =req.header('auth-token');
     if(!token) return res.status(401).send('please provide access token');
 
