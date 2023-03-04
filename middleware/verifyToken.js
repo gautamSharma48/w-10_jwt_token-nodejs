e3c2c5b417103f03b2fedb80168a2b7c3930a5ec
Gautam Sharma <gautamsh@leewayhertz.com>
2023-03-04 19:09:32 +0530

    changes

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,19 +1,17 @@
-const jwt=require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
+const verifyToken = (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) return res.status(401).send("please provide access token");
 
-const  verifyToken=(req,res,next)=>{
-    const token =req.header('auth-token');
-    if(!token) return res.status(401).send('please provide access token');
-
-    try{
-    const verified=jwt.verify(token,process.env.SECRET_KEY);
-    if(!verified) return res.status(400).json({error:"token not valid"})
-    req.user=verified;
+  try {
+    const verified = jwt.verify(token, process.env.SECRET_KEY);
+    if (!verified) return res.status(400).json({ error: "token not valid" });
+    req.user = verified;
     next();
-    }
-    catch(err){
-        res.status(400).json({error:"token not valid"})
-    }
-}
+  } catch (err) {
+    res.status(500).json({ error: "server error" });
+  }
+};
 
-module.exports=verifyToken;
+module.exports = verifyToken;
