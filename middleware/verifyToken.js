128d1332201086c1e5326ddaacfd69ee83d3a849
Gautam Sharma <gautamsh@leewayhertz.com>
2023-03-07 10:44:38 +0530

    changes

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -7,7 +7,7 @@ const verifyToken = (req, res, next) => {
   try {
     const verified = jwt.verify(token, process.env.SECRET_KEY);
     if (!verified) return res.status(400).json({ error: "token not valid" });
-    req.user = verified;
+    req.user =verified;
     next();
   } catch (err) {
     res.status(500).json({ error: "token not valid" });
